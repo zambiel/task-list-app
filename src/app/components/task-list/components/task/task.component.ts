25c0fda6a7c040b5ce5c62c886bb5dfb8aa460c4
Laura Fiestas <100427798+zambiel@users.noreply.github.com>
2025-05-26 00:48:38 +0200

    finish app

--- src/app/components/task-list/components/task/task.component.ts
+++ src/app/components/task-list/components/task/task.component.ts
@@ -1,14 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ITask } from '../../../../models/task.model';
-import { DatePipe } from '@angular/common';
+import { DatePipe, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-task',
-  imports: [DatePipe],
+  imports: [DatePipe, NgClass],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.scss'
+  styleUrl: './task.component.scss',
 })
 export class TaskComponent {
   @Input({ required: true }) task!: ITask;
   @Input() index: number = 0;
+  @Output() remove: EventEmitter<number> = new EventEmitter<number>();
+  @Output() complete: EventEmitter<number> = new EventEmitter<number>();
+
+
+  removeTask() {
+    this.remove.emit(this.index);
+  }
+
+  completeTask() {
+    this.complete.emit(this.index);
+  }
+
+
+
 }
--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -13,6 +13,7 @@ export class TaskListComponent {
   taskListPending: ITask[] = [];
   taskListCompleted: ITask[] = [];
   showInputTask: boolean = false;
+  showCompleted:boolean = false;
 
   errorInputTask: boolean = false;
 
@@ -34,6 +35,21 @@ export class TaskListComponent {
     } else {
       this.errorInputTask = true;
     }
+  }
+
+  removeTask(index: number) {
+    this.taskListPending.splice(index, 1);
+  }
+
+  completeTask(index: number) {
+    const task = this.taskListPending[index];
+    task.completed = true;
+    task.date = new Date();
+    this.taskListPending.splice(index, 1);
+    this.taskListCompleted.push(task);
+  }
+  toggleShowCompleted() {
+    this.showCompleted = !this.showCompleted;
   }
 
 }
